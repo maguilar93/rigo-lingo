e78a6ea7e96df5b2d15bbeb49314cbe4f30c09df
Maria Aguilar <maguilarc@protonmail.com>
2025-05-28 15:35:59 -0600

    main

---
+++ app/src/main/java/com/example/rigolingo/ui/login/AnimatedLoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.rigolingo.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
+import com.example.rigolingo.R
+
+class AnimatedLoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_animated_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val animationView: LottieAnimationView = view.findViewById(R.id.lottie_animation_view)
+        animationView.playAnimation()
+    }
+}
